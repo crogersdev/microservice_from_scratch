34e70d4af7d44b32bd6c34c6c5e43a75d0c481bf
crogersdev <creedrogers@gmail.com>
2020-02-14 12:31:17 -0500

    wip

---
+++ FooSvcImpl.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+file  : api_v1.py
+author: Pat Foo
+why   : implement business logic of REST endpoint
+"""
+
+# python modules
+
+# pip/conda modules
+
+# Foo Inc modules
+
+
+class FooSvcImpl():
+    def __init__(self):
+        pass
+
+    def get_foo(foo_id):
+        pass
+
+    def add_foo(new_foo):
+        pass
